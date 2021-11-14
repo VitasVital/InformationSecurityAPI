e3ce6fea4c6e027a07a8d1600a024a6d605f3a72
Гусев Виталий Евгеньевич <Vitaly.Gusev@norbit.ru>
2021-11-14 17:42:05 +0300

    shifr 5

--- InformationSecurityAPI/Shifrovanie/Shifrovanie5.cs
+++ InformationSecurityAPI/Shifrovanie/Shifrovanie5.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace InformationSecurityAPI.Shifrovanie
@@ -55,12 +56,95 @@ namespace InformationSecurityAPI.Shifrovanie
 
         private string TestMillerRabin(BigInteger _n)
         {
-            return "text";
+            int k = 5; //количество раундов
+            if (_n == 2 || _n == 3)
+            {
+                return "Простое";
+            }
+            if (_n % 2 == 0)
+            {
+                return "Составное";
+            }
+
+            // представим n − 1 в виде (2^s)·t, где t нечётно, это можно сделать последовательным делением n - 1 на 2
+            BigInteger t = _n - 1;
+            int s = 0;
+            while (t % 2 == 0)
+            {
+                t /= 2;
+                s += 1;
+            }
+
+            for (int i = 0; i < k; i++)
+            {
+                // выберем случайное целое число a в отрезке [2, n − 2]
+                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+
+                byte[] _a = new byte[_n.ToByteArray().LongLength];
+
+                BigInteger a;
+
+                do
+                {
+                    rng.GetBytes(_a);
+                    a = new BigInteger(_a);
+                }
+                while (a < 2 || a >= _n - 2);
+
+                BigInteger x = this.VozvedenieStepenPoModulu(a, t, _n);
+
+                // если x == 1 или x == n − 1, то перейти на следующую итерацию цикла
+                if (x == 1 || x == _n - 1)
+                    continue;
+
+                for (int r = 1; r < s; r++)
+                {
+                    // x ← x^2 mod n
+                    x = this.VozvedenieStepenPoModulu(x, 2, _n);
+
+                    // если x == 1, то вернуть "составное"
+                    if (x == 1)
+                        return "Составное";
+
+                    // если x == n − 1, то перейти на следующую итерацию внешнего цикла
+                    if (x == _n - 1)
+                        break;
+                }
+
+                if (x != _n - 1)
+                    return "Составное";
+            }
+
+            return "Вероятно простое";
         }
 
         private string TestFarm(BigInteger _n)
         {
-            BigInteger res = this.VozvedenieStepenPoModulu(3, _n - 1, _n);
+            if (_n == 2 || _n == 3)
+            {
+                return "Простое";
+            }
+            if (_n % 2 == 0)
+            {
+                return "Составное";
+            }
+
+            // выберем случайное целое число a в отрезке [2, n − 2]
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+
+            byte[] _a = new byte[_n.ToByteArray().LongLength];
+
+            BigInteger a;
+
+            do
+            {
+                rng.GetBytes(_a);
+                a = new BigInteger(_a);
+            }
+            while (a < 2 || a >= _n - 2);
+
+            BigInteger res = this.VozvedenieStepenPoModulu(a, _n - 1, _n);
+
             if (res == 1)
             {
                 return "Вероятно простое";
@@ -87,7 +171,7 @@ namespace InformationSecurityAPI.Shifrovanie
                 return textRequest5;
             }
             BigInteger _n = BigInteger.Parse(textRequest5.n);
-            if (_n == 0)
+            if (_n < 2)
             {
                 textRequest5.MillerRabin = "Вы ввели что-то неправильно";
                 textRequest5.SoloveyStrassen = "Вы ввели что-то неправильно";
