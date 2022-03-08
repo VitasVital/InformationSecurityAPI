a065984e6c30da73dcd57a732fd496990532195e
Гусев Виталий Евгеньевич <Vitaly.Gusev@norbit.ru>
2022-03-08 13:38:34 +0300

    ctyptography 4

---
+++ InformationSecurityAPI/Controllers/Cryptography4.cs
@@ -0,0 +1,49 @@
+using InformationSecurityAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InformationSecurityAPI.Shifrovanie;
+using Microsoft.EntityFrameworkCore;
+
+namespace InformationSecurityAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Cryptography4 : Controller
+    {
+        public Cryptography4()
+        {
+        }
+        
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult EncryptionRC4(CryptographyModel4 res)
+        {
+            byte[] key = ASCIIEncoding.ASCII.GetBytes(res.K);
+
+            RC4 encoder = new RC4(key);
+            // string testString = res.Message;
+            byte[] testBytes = ASCIIEncoding.ASCII.GetBytes(res.Message);
+            byte[] result = encoder.Encode(testBytes, testBytes.Length);
+
+            RC4 decoder = new RC4(key);
+            byte[] decryptedBytes = decoder.Decode(result, result.Length);
+            string decryptedString = ASCIIEncoding.ASCII.GetString(decryptedBytes);
+            var response = new 
+            {
+                cryptoMessage = result,
+                decryptedMessage = res.Message
+            };
+            return Json(response);
+        }
+    }
+}
---
+++ InformationSecurityAPI/Models/CryptographyModel4.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InformationSecurityAPI.Models
+{
+    public class CryptographyModel4
+    {
+        public string K { get; set; }
+        public string Message { get; set; }
+    }
+}
